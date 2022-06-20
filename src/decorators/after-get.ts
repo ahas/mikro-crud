617129d2c9203d626311d57a13c9a8697316dc2d
ahas <dev@ahas.io>
2022-06-20 16:14:25 +0900

    Rebuild

--- src/decorators/after-get.ts
+++ src/decorators/after-get.ts
@@ -1,8 +1,8 @@
 import { getMetadataStorage } from "../metadata-storage";
-import { CrudHooks, CrudListenerMetadataArgs } from "../mikro-crud.types";
+import { CrudHooks, CrudListenerMetadataArgs } from "../crud.types";
 
 export function AfterGet(name: string): MethodDecorator {
-    return function (target, propertyName) {
+    return function(target, propertyName) {
         getMetadataStorage().on(name, {
             target,
             propertyName,
--- src/decorators/before-flush.ts
+++ src/decorators/before-flush.ts
@@ -1,8 +1,8 @@
 import { getMetadataStorage } from "../metadata-storage";
-import { CrudHooks, CrudListenerMetadataArgs } from "../mikro-crud.types";
+import { CrudHooks, CrudListenerMetadataArgs } from "../crud.types";
 
 export function BeforeFlush(name: string): MethodDecorator {
-    return function (target, propertyName) {
+    return function(target, propertyName) {
         getMetadataStorage().on(name, {
             target,
             propertyName,
